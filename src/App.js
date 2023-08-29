d08961ff6826911df1f1f9fa488009db984bb2be
Harshit <harshit25102000@gmail.com>
2023-08-29 10:30:48 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/Login';
+
+import {
+  BrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<Login />}/>
+     </Routes>
+      </BrowserRouter>
+  
+  );
+}
+
+export default App;
